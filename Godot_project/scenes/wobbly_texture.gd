50706a69f93265956a65fba0a791485bbab0f038
Marvin <keineidee8@web.de>
2024-09-03 15:55:24 +0200

    better wobble

--- Godot_project/scenes/wobbly_texture.gd
+++ Godot_project/scenes/wobbly_texture.gd
@@ -3,4 +3,9 @@ extends TextureRect
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		material.set_shader_parameter("coff_angle", (get_global_mouse_position()-global_position-size/2.0).angle())
+		var diff = get_global_mouse_position()-global_position-size/2.0
+		material.set_shader_parameter("coff_angle", diff.angle())
+		var x = diff.length()
+		var a = size.x*size.x/2
+		var mul = 0.5*x*exp(-x*x/a)/sqrt(a/(2*exp(1)))
+		material.set_shader_parameter("coff_mul", mul)
